98aaba9e4d426550c2e326c270219153f68f002a
Daniel Norberg <dano@spotify.com>
2014-12-19 16:44:35 +0900

    frame: implement ReferenceCounted directly

--- src/main/java/com/spotify/netty4/handler/codec/zmtp/ZMTPFrame.java
+++ src/main/java/com/spotify/netty4/handler/codec/zmtp/ZMTPFrame.java
@@ -19,38 +19,47 @@ package com.spotify.netty4.handler.codec.zmtp;
 import java.nio.charset.Charset;
 
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.DefaultByteBufHolder;
 import io.netty.buffer.Unpooled;
+import io.netty.util.ReferenceCounted;
 
 import static io.netty.util.CharsetUtil.UTF_8;
 
 
-public class ZMTPFrame extends DefaultByteBufHolder {
+public class ZMTPFrame implements ReferenceCounted {
 
   public static final ZMTPFrame EMPTY_FRAME = new ZMTPFrame(Unpooled.EMPTY_BUFFER);
 
+  private final ByteBuf content;
+
   ZMTPFrame(final ByteBuf content) {
-    super(content);
+    this.content = content;
   }
 
   /**
    * @return Is the current frame empty
    */
   public boolean hasContent() {
-    return content().isReadable();
+    return content.isReadable();
+  }
+
+  /**
+   * Get the frame contents.
+   */
+  public ByteBuf content() {
+    return content;
   }
 
   /**
    * Returns the length of the data
    */
   public int size() {
-    return content().readableBytes();
+    return content.readableBytes();
   }
 
   /**
    * Create a frame from a string, UTF-8 encoded.
    */
-  static public ZMTPFrame from(final String data) {
+  public static ZMTPFrame from(final String data) {
     return from(data, UTF_8);
   }
 
@@ -92,26 +101,51 @@ public class ZMTPFrame extends DefaultByteBufHolder {
   }
 
   @Override
+  public int refCnt() {
+    return content.refCnt();
+  }
+
+  @Override
+  public ReferenceCounted retain() {
+    return content.retain();
+  }
+
+  @Override
+  public ReferenceCounted retain(final int increment) {
+    return content.retain(increment);
+  }
+
+  @Override
+  public boolean release() {
+    return content.release();
+  }
+
+  @Override
+  public boolean release(final int decrement) {
+    return content.release(decrement);
+  }
+
+  @Override
   public boolean equals(final Object o) {
     if (this == o) { return true; }
     if (o == null || getClass() != o.getClass()) { return false; }
 
     final ZMTPFrame zmtpFrame = (ZMTPFrame) o;
 
-    if (content() != null ? !content().equals(zmtpFrame.content()) : zmtpFrame.content() != null) { return false; }
+    if (content != null ? !content.equals(zmtpFrame.content) : zmtpFrame.content != null) { return false; }
 
     return true;
   }
 
   @Override
   public int hashCode() {
-    return content() != null ? content().hashCode() : 0;
+    return content != null ? content.hashCode() : 0;
   }
 
   @Override
   public String toString() {
     return "ZMTPFrame{\"" +
-           ZMTPUtils.toString(content()) +
+           ZMTPUtils.toString(content) +
            "\"}";
   }
 }
